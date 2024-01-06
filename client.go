f98c8e705805087be73e930c930be4c47ce54d89
Sergio Ortega Castillo <sergioortegacastillo@MacBook-Pro-de-Sergio.local>
2024-01-06 11:44:45 -0300

    Minor refactor to client

--- client.go
+++ client.go
@@ -6,76 +6,71 @@ import (
 	"net"
 )
 
-// Estructura que representa al cliente del servicio
 type Client struct {
 	conn net.Conn
 	dec  *json.Decoder
 	enc  *json.Encoder
 }
 
-// Función que crea un nuevo cliente, dado la dirección y el puerto del servicio
 func NewClient(addr string, port string) (*Client, error) {
 	c := new(Client)
+
 	var err error
-	// Crear una conexión TCP con el servicio, usando la función net.Dial
 	c.conn, err = net.Dial("tcp", fmt.Sprintf("%s:%s", addr, port))
 	if err != nil {
 		return nil, err
 	}
-	// Crear un decodificador y un codificador JSON para leer y escribir datos en la conexión
+
 	c.dec = json.NewDecoder(c.conn)
 	c.enc = json.NewEncoder(c.conn)
 	return c, nil
 }
 
 func (c *Client) Insert(key string, value interface{}) (int, error) {
-	// Crear un mensaje con la operación, la clave y el valor
 	msg := map[string]interface{}{
 		"op":    "insert",
 		"key":   key,
 		"value": value,
 	}
-	// Codificar el mensaje en formato JSON y escribirlo en la conexión
+
 	err := c.enc.Encode(msg)
 	if err != nil {
 		return 0, err
 	}
-	// Leer una respuesta del servicio
+
 	var resp map[string]interface{}
 	err = c.dec.Decode(&resp)
 	if err != nil {
 		return 0, err
 	}
-	// Obtener el código de la respuesta
+
 	code, ok := resp["code"].(float64)
 	if !ok {
-		return 0, fmt.Errorf("Código inválido")
+		return 0, fmt.Errorf("invalid code")
 	}
 	return int(code), nil
 }
 
-// Método que busca un valor en el servicio, dado una clave
 func (c *Client) SearchByKey(key string) (interface{}, int, error) {
-	// Crear un mensaje con la operación y la clave
 	msg := map[string]interface{}{
 		"op":  "search_by_key",
 		"key": key,
 	}
-	// Codificar el mensaje en formato JSON y escribirlo en la conexión
+
 	err := c.enc.Encode(msg)
 	if err != nil {
 		return nil, 0, err
 	}
-	// Leer una respuesta del servicio
+
 	var resp map[string]interface{}
 	err = c.dec.Decode(&resp)
 	if err != nil {
 		return nil, 0, err
 	}
-	// Obtener el código y el valor de la respuesta
+
 	code, ok := resp["code"].(float64)
 	if !ok {
-		return nil, 0, fmt.Errorf("Código inválido")
+		return nil, 0, fmt.Errorf("invalid code")
 	}
 	value, ok := resp["value"]
 	if !ok {
@@ -85,26 +80,25 @@ func (c *Client) SearchByKey(key string) (interface{}, int, error) {
 }
 
 func (c *Client) SearchByValue(value string) (interface{}, int, error) {
-	// Crear un mensaje con la operación y la clave
 	msg := map[string]interface{}{
 		"op":    "search_by_value",
 		"value": value,
 	}
-	// Codificar el mensaje en formato JSON y escribirlo en la conexión
+
 	err := c.enc.Encode(msg)
 	if err != nil {
 		return nil, 0, err
 	}
-	// Leer una respuesta del servicio
+
 	var resp map[string]interface{}
 	err = c.dec.Decode(&resp)
 	if err != nil {
 		return nil, 0, err
 	}
-	// Obtener el código y el valor de la respuesta
+
 	code, ok := resp["code"].(float64)
 	if !ok {
-		return nil, 0, fmt.Errorf("Código inválido")
+		return nil, 0, fmt.Errorf("invalid code")
 	}
 	key, ok := resp["key"]
 	if !ok {
@@ -113,60 +107,56 @@ func (c *Client) SearchByValue(value string) (interface{}, int, error) {
 	return key, int(code), nil
 }
 
-// Método que actualiza un valor en el servicio, dado una clave y un valor -- No utilizada por ahora
+// Método no utilizado por ahora
 func (c *Client) Update(key string, value interface{}) (int, error) {
-	// Crear un mensaje con la operación, la clave y el valor
 	msg := map[string]interface{}{
 		"op":    "update",
 		"key":   key,
 		"value": value,
 	}
-	// Codificar el mensaje en formato JSON y escribirlo en la conexión
+
 	err := c.enc.Encode(msg)
 	if err != nil {
 		return 0, err
 	}
-	// Leer una respuesta del servicio
+
 	var resp map[string]interface{}
 	err = c.dec.Decode(&resp)
 	if err != nil {
 		return 0, err
 	}
-	// Obtener el código de la respuesta
+
 	code, ok := resp["code"].(float64)
 	if !ok {
-		return 0, fmt.Errorf("Código inválido")
+		return 0, fmt.Errorf("invalid code")
 	}
 	return int(code), nil
 }
 
-// Método que elimina un valor del servicio, dado una clave
 func (c *Client) Delete(key string) (int, error) {
-	// Crear un mensaje con la operación y la clave
 	msg := map[string]interface{}{
 		"op":  "delete",
 		"key": key,
 	}
-	// Codificar el mensaje en formato JSON y escribirlo en la conexión
+
 	err := c.enc.Encode(msg)
 	if err != nil {
 		return 0, err
 	}
-	// Leer una respuesta del servicio
+
 	var resp map[string]interface{}
 	err = c.dec.Decode(&resp)
 	if err != nil {
 		return 0, err
 	}
-	// Obtener el código de la respuesta
+
 	code, ok := resp["code"].(float64)
 	if !ok {
-		return 0, fmt.Errorf("Código inválido")
+		return 0, fmt.Errorf("invalid code")
 	}
 	return int(code), nil
 }
 
-// Método que cierra la conexión con el servicio
 func (c *Client) Close() error {
 	return c.conn.Close()
 }
